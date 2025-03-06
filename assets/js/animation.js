f935746e8f03e4d015868c6f37dd3f59a33e0011
Manaru0915 <amikasa547@gmail.com>
2025-03-06 12:45:58 +0900

    フッターのheightを変更

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,12 +1,24 @@
-$(function(){
-	$(window).scroll(function (){
-		$('.js-fade').each(function(){
-			var pos = $(this).offset().top;
-			var scroll = $(window).scrollTop();
-			var windowHeight = $(window).height();
-			if (scroll > pos - windowHeight + 100){
-				$(this).addClass('scroll');
-			}
-		});
-	});
+$(document).ready(function() {
+    $('.container').scroll(function() {
+        $('.js-fade').each(function() {
+            var pos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > pos - windowHeight + 100) {
+                $(this).addClass('scroll');
+            }
+        });
+
+        let scroll = $('.container').scrollTop();
+        console.log(scroll);
+        if (scroll >= 1310) {
+            $('#smooth1').addClass('is-animated');
+        }
+				if (scroll >= 4800) {
+            $('#smooth2').addClass('is-animated');
+        }
+				if(scroll >= 8300) {
+            $('#smooth3').addClass('is-animated');
+        }
+    });
 });
